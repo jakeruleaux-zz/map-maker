f4016cc56b1e85e3d805fae8129eec0d1a44b09a
jacob ruleaux <jakeruleaux@gmail.com>
2019-02-23 15:41:25 -0800

    add drop component

---
+++ src/Drop.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import classNames from 'classNames';
+import Dropzone from 'react-dropzone';
+
+class MyDropzone extends React.Component {
+    onDrop = (acceptedFiles, rejectedFiles) => {
+        // Do something with files
+    }
+
+    render() {
+        return (
+            <Dropzone onDrop={this.onDrop}>
+                {({getRootProps, getInputProps, isDragActive}) => {
+                    return (
+                        <div
+                            {...getRootProps()}
+                            className={classNames('dropzone', {'dropzone--isActive': isDragActive})}
+                        >
+                            <input {...getInputProps()} />
+                            {
+                                isDragActive ?
+                                    <p>Drop files here...</p> :
+                                    <p>Try dropping some files here, or click to select files to upload.</p>
+                            }
+                        </div>
+                    )
+                }}
+            </Dropzone>
+        );
+    }
+}
+
+export default Dropzone;
